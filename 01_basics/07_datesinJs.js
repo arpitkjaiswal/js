3fa72cea488d9b51f05b48b0ac44ddb7acffce6c
Arpit Kumar Jaiswal <arpit1.mitmpl2024@learner.manipal.edu>
2026-06-21 11:33:18 +0000

    Date and time

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,27 @@
+// dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(typeof myDate);
+
+
+
+
+let myTimeStamp =Date.now();
+console.log(myTimeStamp);
+console.log(myDate.getTime());
+
+const d1 = new Date("2026-06-21");
+const d2 = new Date("2026-06-25");
+
+console.log(d1 < d2);
+
+const date = new Date();
+
+console.log(date.toDateString());
+
+
+
+  
