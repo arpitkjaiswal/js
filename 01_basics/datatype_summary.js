d80ee23338f5a3046a0aa5743d7dbedcf392d37d
Arpit Kumar Jaiswal <arpit1.mitmpl2024@learner.manipal.edu>
2026-06-21 09:07:04 +0000

    data type summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,41 @@
+// # Primitive
+
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const andotherId = Symbol('123')
+
+console.log(id === andotherId);
+
+const bigNumber=456235234263488n
+
+console.log(typeof bigNumber);
+
+
+// Reference (Non primitive)
+
+// Arrays, Objects, Functions
+
+
+const heros = ["batman", "SuperHero", "Spiderman"]
+
+let myObj={
+    name: "Arpit",
+    Age: 19,
+}
+
+const myFunction = function(){
+
+    console.log("Hello world");
+    
+}
+
