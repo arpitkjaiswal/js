d7d87b4b3c84595e38f6253375e3598a56a34c32
Arpit Kumar Jaiswal <arpit1.mitmpl2024@learner.manipal.edu>
2026-06-21 16:55:15 +0000

    Objects Part2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,55 @@
+// const tinderUser = {}; an empty object created using object literal syntax
+
+
+
+const tinderUser = new Object(); //an empty object created using the Object constructor
+tinderUser.id = "123abc";
+tinderUser.name = "Arpit";
+tinderUser.isLoggedIn = false;
+
+console.log(tinderUser)
+
+const regularUser = {
+    email: "some@example.com",
+    fullname: {
+        userFullName: {
+            firstName: "Arpit",
+            lastName: "Jaiswal"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userFullName.firstName) // Arpit
+
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "c", 4: "d"};
+
+const obj3 = Object.assign({}, obj1, obj2) // merging two objects into a new object , 
+// {} is used to create a new object and assign the properties of obj1 and obj2 to it    
+console.log(obj3) // { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+const obj4 = {...obj1, ...obj2} // merging two objects into a new object using spread operator
+console.log(obj4) // { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+const user = [
+    {
+        id: 1,
+        email: "some@example.com"
+    },
+    {
+        id: 2,
+        email: "another@example.com"
+    }
+] // array of objects
+
+// user[1].email just like we access the array elements using index,
+// we can access the object properties using dot notation or bracket notation 
+console.log(tinderUser)
+
+
+console.log(Object.keys(tinderUser)) // returns an array of keys of the object
+console.log(Object.values(tinderUser)) // returns an array of values of the object
+console.log(Object.entries(tinderUser)) // returns an array of key-value pairs of the object
+
+console.log(tinderUser.hasOwnProperty("name")) // returns true if the object has the property, false otherwise
