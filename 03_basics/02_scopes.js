349abacc76c0026d07673c08c9eda6084bdfd7e5
Arpit Kumar Jaiswal <arpit1.mitmpl2024@learner.manipal.edu>
2026-06-22 09:12:02 +0000

    scope updated

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,44 @@
 function test() {
     var x = 10;
-    console.log(x);
+    // console.log(x);
 }
 
 test();
 // console.log(x); // Error
+
+
+function one(){
+    const username = "John";
+
+    function two(){
+        const website = "example.com";
+        // console.log(username);
+        // console.log(website);
+    }
+    two();
+}
+one();
+
+// if (true) {
+//     const username = "Alice";
+//     if(username === "Alice") {
+//         const message = "Hello, Alice!";
+//         console.log(message);
+//     }
+//     // console.log(message); // Error
+// }
+// // console.log(username); // Error
+
+
+//+++++++++++++++++++++++++ Interesting example +++++++++++++++++++++++++
+
+function addone(value){
+    return value + 1;
+}
+addone(5); // 6
+
+
+const addtwo = function(value){
+    return value + 2;
+}
+addtwo(5); // 7
